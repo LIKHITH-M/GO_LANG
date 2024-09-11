2874d8573793ec875b3149625f0c232652107529
LIKHITH-M <165349349+LIKHITH-M@users.noreply.github.com>
2024-09-11 18:30:47 +0530

    added comments to the hello world program

--- hello_world.go
+++ hello_world.go
@@ -1,7 +1,8 @@
-package main
-
+//main package to create the exicutable file
+ package main
+// importing the format package
 import "fmt"
-
+//main fulction
 func main() {
     fmt.Println("Hello, World!")
 }
