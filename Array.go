96443f915af735198c082054e0c62f6211b95c82
LIKHITH-M <165349349+LIKHITH-M@users.noreply.github.com>
2024-09-16 11:37:47 +0530

    types of initialization and declaration of an array

---
+++ Array.go
@@ -0,0 +1,21 @@
+package main 
+
+import "fmt"
+
+func main(){
+	fmt.Println("array program")
+	
+	//basic type of initialising and declaring an array.
+	var arr = [3]int{1,2,3} //the value in the square braces represent the size.
+	fmt.Println(arr) //printing complete array.
+	//output [1,2,3]
+
+
+	//using the colon operator.
+	arr2 := [4]int{2,4,5} //here the 3rd index by default initialized to 0.
+	fmt.Println(arr2) // output [2,4,5,0]
+
+	//using the period operator for size.
+	arr3 := [...]int{2,34,5,6,62,2,4} //size is assigned wrt to number of array elements.
+	fmt.Println(arr3)
+}
