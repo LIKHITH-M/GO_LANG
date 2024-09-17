0365d614e02e43758b3324876837afdb2029ed89
LIKHITH-M <165349349+LIKHITH-M@users.noreply.github.com>
2024-09-17 22:04:10 +0530

    added a comment on difference between slice and array syntax

--- Slice.go
+++ Slice.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
     // Define a slice of integers
-    numbers := []int{10, 20, 30, 40, 50}
+    numbers := []int{10, 20, 30, 40, 50} //the difference between slice and array is we mention the size for array but not for slice!!!!!.
 
     // Print the length and capacity of the slice
     fmt.Printf("Length: %d, Capacity: %d\n\n", len(numbers), cap(numbers))
